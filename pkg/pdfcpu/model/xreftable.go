8accdfb80533a6483658702bfc983109913d16cb
Horst Rutter <hhrutter@gmail.com>
2025-05-08 10:42:54 +0200

    Fix #1149
    (cherry picked from commit f6312869d058fca3b3277c873067a3676e587f11)

--- pkg/pdfcpu/model/xreftable.go
+++ pkg/pdfcpu/model/xreftable.go
@@ -176,6 +176,7 @@ type XRefTable struct {
 }
 
 // NewXRefTable creates a new XRefTable.
+// TODO Export
 func newXRefTable(conf *Configuration) (xRefTable *XRefTable) {
 	return &XRefTable{
 		Table:             map[int]*XRefTableEntry{},
